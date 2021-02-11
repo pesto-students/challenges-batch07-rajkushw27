03bffa986ed3f5e7f617024df0acf171ff60f93c
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-11 14:45:16 +0530

    feat: add duplicateLetters funtion

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,7 +1,24 @@
 function duplicateLetters(...args) {
-  return args;
+  const str = args[0];
+
+  const seen = new Map();
+
+  const Intialcount = 1;
+  let max = 1;
+
+  for (const char of str) {
+    if (seen.has(char)) {
+      seen.set(char, seen.get(char) + 1);
+    } else seen.set(char, Intialcount);
+  }
+
+  for (const value of seen.values()) {
+    if (max < value) {
+      max = value;
+    }
+  }
+  if (max === 1) return false;
+  return max;
 }
 
-export {
-  duplicateLetters,
-};
+export { duplicateLetters };
