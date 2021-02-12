68ce724a5003303c73a469b7b5fbff8926717ec0
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-12 12:03:07 +0530

    feat: rot13 cipher funtion implemented

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,5 +1,24 @@
-function rot13(...args) {
-  return args;
+function rot13(str) {
+  const shift = 13;
+  const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let cipherText = '';
+
+  let i = 0;
+  while (i < str.length) {
+    if (charSet.indexOf(str[i]) !== -1) {
+      const charIndex = charSet.indexOf(str[i]);
+      if (charSet[charIndex + shift]) {
+        cipherText += charSet[charIndex + shift];
+      } else {
+        cipherText += charSet[charIndex + shift - 26];
+      }
+    } else {
+      cipherText += str[i];
+    }
+
+    i += 1;
+  }
+  return cipherText;
 }
 
 export {
