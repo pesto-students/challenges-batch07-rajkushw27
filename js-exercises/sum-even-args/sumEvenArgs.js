e734ce531996ec6e6f3b97f558885974e2b7d3ab
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-13 12:28:50 +0530

    feat: sum even argument function implemention

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,3 +1,9 @@
-const sumEvenArgs = (...args) => args;
+const sumEvenArgs = (...args) => {
+  let sum = 0;
+  for (const value of args) {
+    if (value % 2 === 0) { sum += value; }
+  }
+  return sum;
+};
 
 export { sumEvenArgs };
