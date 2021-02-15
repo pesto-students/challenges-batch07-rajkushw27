80138966cd0766398ee00820e8b8d1a5a926b380
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-15 22:28:02 +0530

    feat: balanceBraces funtion implementation

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,5 +1,29 @@
 function balancedBraces(...args) {
-  return args;
+  const inputString = args[0];
+  const stack = [];
+
+  const open = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  };
+
+  const closed = {
+    '}': true,
+    ']': true,
+    ')': true,
+  };
+
+  for (let i = 0; i < inputString.length; i += 1) {
+    const char = inputString[i];
+
+    if (open[char]) {
+      stack.push(char);
+    } else if (closed[char]) {
+      if (open[stack.pop()] !== char) return false;
+    }
+  }
+  return stack.length === 0;
 }
 
 export {
