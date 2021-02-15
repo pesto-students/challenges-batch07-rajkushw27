156b05ac34804ce041cfd1721fe1cf1bb45e1310
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-15 21:45:54 +0530

    feat: curry function implementation

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,4 +1,11 @@
-function curry() {
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+
+    return (...args2) => curried.apply(this, args.concat(args2));
+  };
 }
 
 export {
