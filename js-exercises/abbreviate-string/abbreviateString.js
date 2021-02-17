22242816a269b0c98d19202e459be0d9b5735a31
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-17 14:13:49 +0530

    update: code refactor

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,14 +1,11 @@
 function abbreviateString(str) {
   const strArray = str.split(' ');
 
-  let outputArray = strArray[0].charAt(0).toUpperCase() + strArray[0].slice(1);
+  const firstName = strArray[0].charAt(0).toUpperCase() + strArray[0].slice(1);
+  const shortLastName = `${strArray[strArray.length - 1][0]}.`;
 
-  const endName = strArray[strArray.length - 1];
-
-  const shortEndName = `${endName[0]}.`;
-
-  outputArray = `${outputArray} ${shortEndName.toUpperCase()}`;
-  return outputArray;
+  const abbreviateName = `${firstName} ${shortLastName.toUpperCase()}`;
+  return abbreviateName;
 }
 
 export { abbreviateString };
