a298598c347293885cd0691a0167c6d99c254547
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-17 14:07:29 +0530

    udpate: code refactor

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,24 +1,18 @@
 function duplicateLetters(...args) {
   const str = args[0];
 
-  const seen = new Map();
-
-  const Intialcount = 1;
-  let max = 1;
+  const charMap = {};
 
   for (const char of str) {
-    if (seen.has(char)) {
-      seen.set(char, seen.get(char) + 1);
-    } else seen.set(char, Intialcount);
+    if (!charMap[char]) charMap[char] = 1;
+    else charMap[char] += 1;
   }
 
-  for (const value of seen.values()) {
-    if (max < value) {
-      max = value;
-    }
-  }
-  if (max === 1) return false;
-  return max;
+  const CharCounts = Object.values(charMap);
+  const maxCharCount = Math.max(...CharCounts);
+
+  if (!maxCharCount || maxCharCount === 1) return false;
+  return maxCharCount;
 }
 
 export { duplicateLetters };
