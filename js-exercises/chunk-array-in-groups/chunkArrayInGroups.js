8dbf7d39c15567768cc2ee1338c9f91a354f64fa
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-19 14:04:42 +0530

    feat: chunkArray function implementation

--- js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,5 +1,8 @@
 function chunkArrayInGroups(array, chunkSize) {
-  return [array, chunkSize];
+  return array.reduce((twoDArray, value, index) => {
+    if (index % chunkSize === 0) twoDArray.push(array.slice(index, index + chunkSize));
+    return twoDArray;
+  }, []);
 }
 
 export {
