296c22884cb69b492e48d483a598f1ada1b49668
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-19 09:14:21 +0530

    update: code refactor

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,9 +1,4 @@
-const sumEvenArgs = (...args) => {
-  let sum = 0;
-  for (const value of args) {
-    if (value % 2 === 0) { sum += value; }
-  }
-  return sum;
-};
+const sumEvenArgs = (...args) => args.filter((value) => value % 2 === 0)
+  .reduce((acc, value) => acc + value, 0);
 
 export { sumEvenArgs };
