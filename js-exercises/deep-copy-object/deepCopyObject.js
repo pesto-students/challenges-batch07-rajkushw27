ca63e850be889abdf2f4c33eb73f2355757f20d5
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-19 14:48:24 +0530

    feat: deep copy funtion implementation

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,3 +1,11 @@
-const deepCopyObject = objToCopy => objToCopy;
+const deepCopyObject = objToCopy => {
+  const deepCopy = {};
+  for (const obj in objToCopy) {
+    if (typeof (objToCopy[obj]) === 'object' && objToCopy[obj] != null) {
+      deepCopy[obj] = deepCopyObject(deepCopy[obj]);
+    } else deepCopy[obj] = objToCopy[obj];
+  }
+  return deepCopy;
+};
 
 export { deepCopyObject };
