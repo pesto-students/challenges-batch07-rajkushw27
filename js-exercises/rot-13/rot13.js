17b7e6ce930e5ca713c0df9cc5a9f00cf84e50fe
rajkushw27 <rajkushwaha2705@gmail.com>
2021-02-19 09:10:43 +0530

    udpate: code refactor

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,24 +1,5 @@
 function rot13(str) {
-  const shift = 13;
-  const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let cipherText = '';
-
-  let i = 0;
-  while (i < str.length) {
-    if (charSet.indexOf(str[i]) !== -1) {
-      const charIndex = charSet.indexOf(str[i]);
-      if (charSet[charIndex + shift]) {
-        cipherText += charSet[charIndex + shift];
-      } else {
-        cipherText += charSet[charIndex + shift - 26];
-      }
-    } else {
-      cipherText += str[i];
-    }
-
-    i += 1;
-  }
-  return cipherText;
+  return str.replace(/[A-Z]/g, (s) => String.fromCharCode(s.charCodeAt() + (s.toUpperCase() < 'N' ? 13 : -13)));
 }
 
 export {
