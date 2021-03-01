cad013208f5de387e3c7444992885ee0225712ab
RAJKUSHW <RAJKUSHW@rajkushw.in.oracle.com>
2021-03-01 16:14:58 +0530

    feat: allPromise function implementation

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,3 +1,19 @@
-const allPromises = args => {};
+function allPromises(items) {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    let counter = 0;
+    items.forEach((item) => {
+      Promise.resolve(item)
+        .then((res) => {
+          counter += 1;
+          result.push(res);
+          if (counter === items.length) {
+            resolve(result);
+          }
+        })
+        .catch((error) => reject(error));
+    });
+  });
+}
 
 export { allPromises };
