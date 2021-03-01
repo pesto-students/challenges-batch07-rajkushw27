7222e47ad873c30ea6a28b693fb4dd8fcac01674
RAJKUSHW <RAJKUSHW@rajkushw.in.oracle.com>
2021-03-01 16:35:52 +0530

    feat: All settledPromise function implementation

--- js-exercises/all-settled/allSettled.js
+++ js-exercises/all-settled/allSettled.js
@@ -1,2 +1,21 @@
-
+function allSettled(promises) {
+  return new Promise((resolve) => {
+    if (promises === undefined) return undefined;
+    const result = [];
+    promises.forEach((value, index) => {
+      Promise.resolve(value)
+        .then((res) => {
+          result[index] = { status: 'fulfilled', value: res };
+          if (index === promises.length - 1) resolve(result);
+        })
+        .catch((error) => {
+          result[index] = { status: 'rejected', reason: error };
+          if (index === promises.length - 1) {
+            resolve(result);
+          }
+        });
+    });
+    return null;
+  });
+}
 export { allSettled };
