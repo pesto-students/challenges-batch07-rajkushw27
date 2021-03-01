736a69e201dc8aa9d4eda21edd56d3b12faeeacc
RAJKUSHW <RAJKUSHW@rajkushw.in.oracle.com>
2021-03-01 11:59:41 +0530

    feat: Debounce funtion implementation

--- js-exercises/debounce/debounce.js
+++ js-exercises/debounce/debounce.js
@@ -1,3 +1,14 @@
-function debounce(fn, timeInMs) {}
+function debounce(callback, wait) {
+  let stopWatch = null;
+  return (...args) => {
+    if (stopWatch) {
+      clearTimeout(stopWatch);
+    }
+    stopWatch = setTimeout(() => {
+      callback.apply(this, args);
+      stopWatch = null;
+    }, wait);
+  };
+}
 
 export { debounce };
